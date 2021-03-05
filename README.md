8f63122ea9add542465a8b0ae0a62f8271e62285
L0tus-Program <62225877+L0tus-Program@users.noreply.github.com>
2021-03-05 20:13:12 -0300

    Versão base
    Versão base do ytdownload

---
+++ README.md
@@ -0,0 +1,5 @@
+# download-youtube
+
+Código para realizar o download de vídeos do YouTube
+
+Caso não tenha os requirimentos instalados basta rodar "pip install -r requirements.txt" 
---
+++ ytdownload.py
@@ -0,0 +1,13 @@
+from pytube import YouTube
+import pyautogui
+link = pyautogui.prompt("Digite o link que deseja baixar") # Inserir link do vídeo
+path = pyautogui.prompt("Digite o diretório que deseja salvar o vídeo:  ") # Inserir local de download 
+yt = YouTube(link)
+pyautogui.alert("Estou baixando seu vídeo ! Por favor , aguarde um instante . \nTítulo : %s" %(yt.title) ) #Mostra junto da mensagem de download o título do vídeo
+ys = yt.streams.get_highest_resolution() #Seleciona a melhor qualidade de vídeo disponível
+ys.download(path) # Path é a pasta para salver o vídeo , passada como parâmetro para a função download.
+pyautogui.alert("Download completo ! \nVídeo salvo em %s" %(path))
+
+# https://www.youtube.com/watch?v=ex86DhcJ_UM
+
+
